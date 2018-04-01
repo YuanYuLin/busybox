1b26de2fc3f3d48fb3f9414a0a058c07652e2ab4
yyl <yuanyu.lin@gmail.com>
2018-04-02 07:29:57 +0800

    MAJOR_NUM=1 MINOR_NUM=9 AUX_NUM=0 update package

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -6,8 +6,9 @@ output_dir = ""
 busybox_tarball = ""
 busybox_def_config = ""
 busybox_build_dir = ""
+busybox_install_dir = ""
 src_busybox_udhcpc_script = ""
-tmp_busybox_udhcpc_script = ""
+src_busybox_inittab = ""
 jobs_count = ""
 
 def set_global(args):
@@ -16,8 +17,9 @@ def set_global(args):
     global busybox_tarball
     global busybox_def_config
     global busybox_build_dir
+    global busybox_install_dir
     global src_busybox_udhcpc_script
-    global tmp_busybox_udhcpc_script
+    global src_busybox_inittab
     global jobs_count
 
     pkg_args = args["pkg_args"]
@@ -29,8 +31,10 @@ def set_global(args):
     busybox_tarball = ops.path_join(pkg_path, BUSYBOX_VERSION) + ".tar.bz2"
     busybox_def_config = ops.path_join(pkg_path, def_cfg_version)
     busybox_build_dir = ops.path_join(output_dir, BUSYBOX_VERSION)
-    src_busybox_udhcpc_script = ops.path_join(busybox_build_dir, "examples/udhcp/simple.script")
-    tmp_busybox_udhcpc_script = ops.path_join(output_dir, "default.script")
+    busybox_install_dir = ops.path_join(output_dir, "busybox-bin")
+    #src_busybox_udhcpc_script = ops.path_join(busybox_build_dir, "examples/udhcp/simple.script")
+    src_busybox_udhcpc_script = ops.path_join(pkg_path, "default.script")
+    src_busybox_inittab = ops.path_join(pkg_path, "inittab")
     #dst_busybox_udhcpc_script_dir = iopc.getBinPkgPath(args["pkg_name"]) + "/usr/share/udhcpc/"
     if jobs_count == "" :
         jobs_count = "2"
@@ -80,12 +84,22 @@ def MAIN_BUILD(args):
 def MAIN_INSTALL(args):
     set_global(args)
 
+    ops.mkdir(busybox_install_dir)
     extra_conf = []
-    extra_conf.append("CONFIG_PREFIX=" + iopc.getBinPkgPath(args["pkg_name"]))
+    #extra_conf.append("CONFIG_PREFIX=" + iopc.getBinPkgPath(args["pkg_name"]))
+    extra_conf.append("CONFIG_PREFIX=" + busybox_install_dir)
     iopc.make_install(busybox_build_dir, extra_conf)
 
-    ops.copyto(src_busybox_udhcpc_script, tmp_busybox_udhcpc_script)
-    iopc.installBin(args["pkg_name"], tmp_busybox_udhcpc_script, "/usr/share/udhcpc/")
+    ops.mkdir(ops.path_join(busybox_install_dir, "usr/share/udhcpc"))
+    ops.copyto(src_busybox_udhcpc_script, ops.path_join(busybox_install_dir, "usr/share/udhcpc"))
+
+    ops.mkdir(ops.path_join(busybox_install_dir, "etc"))
+    ops.copyto(src_busybox_inittab, ops.path_join(busybox_install_dir, "etc"))
+    ops.ln(busybox_install_dir, "/sbin/init", "init")
+    #iopc.installBin(args["pkg_name"], tmp_busybox_udhcpc_script, "/usr/share/udhcpc/")
+    #iopc.installBin(args["pkg_name"], dst_busybox_inittab, "/etc")
+    iopc.installBin(args["pkg_name"], ops.path_join(busybox_install_dir, "."), ".")
+    #ops.ln(install_dir, "/sbin/init", "init")
     return False
 
 def MAIN_CLEAN_BUILD(args):
